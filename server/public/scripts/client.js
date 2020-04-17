9965718bd6b7c2a30230d1848defd4e403b71c14
Dev Jana <devjana@Devs-MBP-2.domain>
2020-04-17 14:30:12 -0500

    get and post working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,28 @@
 $( document ).ready( onReady );
 
+function addItem(){
+    console.log( 'in addItem' );
+    // get user input and put into an object
+    let objectToSend = {
+        size: $( '#sizeIn' ).val(),
+        color: $( '#colorIn' ).val(),
+        name: $( '#nameIn' ).val()
+    }
+    console.log( 'sending:', objectToSend );
+    // send to server via AJAX POST
+    $.ajax({
+        type: 'POST',
+        url: '/inventory',
+        data: objectToSend
+    }).then( function( response ){
+        console.log( 'back from POST:', response );
+        // update DOM
+    }).catch( function( err ){
+        alert( 'error posting item. see console for details' );
+        console.log( err );
+    }) //end AJAX
+} // end addItem
+
 function onReady(){
-    console.log( 'JQ' );
+    $( '#addItemButton' ).on( 'click', addItem );
 } // end onReady
--- server/server.js
+++ server/server.js
@@ -7,8 +7,19 @@ app.use( express.static( 'server/public' ) );
 app.use( bodyParser.urlencoded( { extended: true } ) );
 // globals
 const port = 5000;
+let inventory = [];
 // spin up server
 app.listen( port, ()=>{
     console.log( 'server up:', port );
 })
 // routes
+app.get( '/inventory', ( req, res )=>{
+    console.log( 'in /inventory GET' );
+    res.send( inventory );
+}) // end /inventory GET
+
+app.post( '/inventory', ( req, res )=>{
+    console.log( 'in inventory POST:', req.body );
+    inventory.push( req.body );
+    res.sendStatus( 201 );
+}) // end inventory POST
